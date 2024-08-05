ef91d2a706abc2baefb600da92d17f1944126714
Nkwor-Jane <nkwor.jane.chinelo@gmail.com>
2024-08-06 00:44:00 +0100

    Done with the main task, need to make some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
-
+import WeatherApp from './components/WeatherApp'
 function App() {
 
   return (
-    <div className='bg-red-500'>
-      <h1 className='text-white'>Hello</h1>
+    <div>
+      <WeatherApp/>
     </div>
   )
 }
